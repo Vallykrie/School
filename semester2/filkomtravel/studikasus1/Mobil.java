1a7c051404c50013fa688d6c726a051b952f4be9
Nathan Sudiara <nathansudiara@gmail.com>
2024-03-11 23:06:44 +0700

    Add vehicle type-specific constructors

--- semester2/filkomtravel/studikasus1/Mobil.java
+++ semester2/filkomtravel/studikasus1/Mobil.java
@@ -1,10 +1,22 @@
 package filkomtravel.studikasus1;
 
+/**
+ * The `Mobil` class represents a type of vehicle called "Mobil".
+ * It extends the `Vehicle` class and inherits its properties and methods.
+ */
 public class Mobil extends Vehicle {
     final String type = "Mobil";
 
+    /**
+     * Constructs a new `Mobil` object with the specified model, color, number plate, capacity, and price.
+     *
+     * @param model       the model of the mobil
+     * @param color       the color of the mobil
+     * @param numberPlate the number plate of the mobil
+     * @param capacity    the capacity of the mobil
+     * @param price       the price of the mobil
+     */
     Mobil(String model, String color, String numberPlate, int capacity, double price) {
         super(model, color, numberPlate, capacity, price);
     }
-
 }
--- semester2/filkomtravel/studikasus1/Motor.java
+++ semester2/filkomtravel/studikasus1/Motor.java
@@ -1,10 +1,23 @@
 package filkomtravel.studikasus1;
 
-public class Motor extends Vehicle{
+/**
+ * Represents a Motor vehicle.
+ * 
+ * This class extends the Vehicle class and provides additional functionality specific to Motor vehicles.
+ */
+public class Motor extends Vehicle {
     final String type = "Motor";
 
+    /**
+     * Constructs a new Motor object with the specified model, color, number plate, capacity, and price.
+     * 
+     * @param model       the model of the Motor vehicle
+     * @param color       the color of the Motor vehicle
+     * @param numberPlate the number plate of the Motor vehicle
+     * @param capacity    the capacity of the Motor vehicle
+     * @param price       the price of the Motor vehicle
+     */
     Motor(String model, String color, String numberPlate, int capacity, double price) {
         super(model, color, numberPlate, capacity, price);
     }
-
 }
