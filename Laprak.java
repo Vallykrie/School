f64f57723017db1a1815303255df5e971356b850
Nathan Sudiara <nathansudiara@gmail.com>
2024-03-12 08:11:22 +0700

    Add Laprak.java file

---
+++ Laprak.java
@@ -0,0 +1,11 @@
+public class Laprak {
+
+    // Variable Instance
+    String a = "ini adalah variable instance";
+
+    public void lokal(){
+        
+        // Variable Lokal
+        String b = "ini adalah variable lokal";
+    }
+}
