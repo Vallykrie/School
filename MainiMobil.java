ff0423b839a0b5db0432a74c6dc2c1c1aabb45ce
Nathan Sudiara <nathansudiara@gmail.com>
2024-03-12 08:54:04 +0700

    Update user input handling in MainiMobil.java

--- MainiMobil.java
+++ MainiMobil.java
@@ -6,12 +6,13 @@ public class MainiMobil {
         Scanner than = new Scanner(System.in);
         System.out.println("Masukkan kecepatan mobil : ");
         int kecepatan = than.nextInt();
+        than.nextLine();
         System.out.println("Masukkan manufaktur mobil : ");
-        String manufaktur = than.next();
+        String manufaktur = than.nextLine();
         System.out.println("Masukkan no plat mobil : ");
-        String noPlat = than.next();
+        String noPlat = than.nextLine();
         System.out.println("Masukkan warna mobil : ");
-        String warna = than.next();
+        String warna = than.nextLine();
 
         Mobil m1 = new Mobil();
         m1.setKecepatan(kecepatan);
