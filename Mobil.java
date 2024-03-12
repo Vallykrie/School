1fc17a801b32c469d012358ae4e0e4d9cb76c89a
Nathan Sudiara <nathansudiara@gmail.com>
2024-03-12 09:35:35 +0700

    Refactor Mobil class to include additional properties and methods

--- Mobil.java
+++ Mobil.java
@@ -1,40 +1,42 @@
-public class Mobil {
-    private String noPlat;
-    private String warna;
-    private String manufaktur;
-    private int kecepatan;
-    private double waktu;
+    public class Mobil {
+        private String noPlat;
+        private String warna;
+        private String manufaktur;
+        private int kecepatan;
+        private double waktu;
+        private double jarak;
 
-    public void setNoPlat(String s) {
-        noPlat = s;
+        public void setNoPlat(String s) {
+            noPlat = s;
+        }
+        public void setWarna(String s) {
+            warna = s;
+        }
+        public void setManufaktur(String s) {
+            manufaktur = s;
+        }
+        public void setKecepatan(int i) {
+            kecepatan = i;
+            double kecepatanMs = rubahKecepatan(kecepatan);
+        }
+        public void setWaktu(double d) {
+            waktu = d;
+            double sekon = rubahSekon(waktu);
+        }
+        private double rubahSekon(double waktu){
+            return waktu*3600;
+        }
+        private double rubahKecepatan(int kecepatan){
+            return kecepatan*1000/3600;
+        }
+        public void hitungJarak(int kecepatan, double waktu){
+            jarak = kecepatan*waktu;
+        }
+        public void displayMessage() {
+            System.out.println("Mobil anda adalah bermerek " + manufaktur);
+            System.out.println("mempunyai nomor plat " + noPlat);
+            System.out.println("serta memililki warna " + warna);
+            System.out.println("jarak yang dapat ditempuh " + jarak/1000 + " km");
+            System.out.println("dan mampu menempuh kecepatan " + kecepatan);
+        }
     }
-    public void setWarna(String s) {
-        warna = s;
-    }
-    public void setManufaktur(String s) {
-        manufaktur = s;
-    }
-    public void setKecepatan(int i) {
-        kecepatan = i;
-        double kecepatanMs = rubahKecepatan(kecepatan);
-    }
-    public void setWaktu(double d) {
-        waktu = d;
-        double sekon = rubahSekon(waktu);
-    }
-    private double rubahSekon(double waktu){
-        return waktu*3600;
-    }
-    private double rubahKecepatan(int kecepatan){
-        return kecepatan*1000/3600;
-    }
-    public void hitungJarak(int kecepatan, int waktu){
-        double jarak = kecepatan*waktu;
-    }
-    public void displayMessage() {
-        System.out.println("Mobil anda adalah bermerek " + manufaktur);
-        System.out.println("mempunyai nomor plat " + noPlat);
-        System.out.println("serta memililki warna " + warna);
-        System.out.println("dan mampu menempuh kecepatan " + kecepatan);
-    }
-}
