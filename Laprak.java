f30449907a52a2840b664f5750082fbe6d7bbaee
Nathan Sudiara <nathansudiara@gmail.com>
2024-03-12 09:55:24 +0700

    Add method to calculate time and total words to finish a book

--- Laprak.java
+++ Laprak.java
@@ -1,11 +1,21 @@
 public class Laprak {
+    /**
+     * Mahasiswa A ingin menulis pada sebuah buku tulis yang ingin dia miliki, 
+     * isi lembar buku tersebut adalah 50 lembar. 
+     * Setiap harinya ia menulis sebanyak 100 kata perhari yang cukup untuk 1/2 halaman buku. 
+     * Buatlah rumus untuk menghitung berapa lama ia menghabiskan 1 buku tersebut serta identifikasilah objek, 
+     * dan karakteristiknya kemudian implementasikan dalam bentuk class.
+     */
+    
+    private int lembarBuku = 50;
+    private int kataPerHari = 100;
+    private double menulisPerHari = 0.5;
 
-    // Variable Instance
-    String a = "ini adalah variable instance";
-
-    public void lokal(){
-        
-        // Variable Lokal
-        String b = "ini adalah variable lokal";
+    public void lamaMenghabiskanBuku(){
+        double waktuMenghabiskanBuku = lembarBuku/menulisPerHari;
+        double totalKata = waktuMenghabiskanBuku*kataPerHari;
+        System.out.println("Waktu yang dibutuhkan untuk menghabiskan buku adalah " + waktuMenghabiskanBuku + " hari");
+        System.out.println("Total kata yang ditulis adalah " + totalKata + " kata");
     }
+
 }
--- semester2/test.java
+++ semester2/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-
+        Laprak test = new Laprak();
+        test.lamaMenghabiskanBuku();
     }
 }
