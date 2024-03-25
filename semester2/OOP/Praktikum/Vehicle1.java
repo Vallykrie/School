5579fe28467ac367577c3db25bef8285abd38195
Nathan Sudiara <nathansudiara@gmail.com>
2024-03-25 16:41:16 +0700

    Refactor Vehicle1 class and update TestVehicle1.java

--- semester2/OOP/Praktikum/Vehicle1.java
+++ semester2/OOP/Praktikum/Vehicle1.java
@@ -1,7 +1,7 @@
 package OOP.Praktikum;
 
 public class Vehicle1 {
-    private double load, maxLoad;
+    protected double load, maxLoad;
 
     public Vehicle1(double max) {
         this.maxLoad = max;
