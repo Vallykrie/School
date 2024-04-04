8867883c555425f84bff02c7b80313f2755736fc
Nathan Sudiara <nathansudiara@gmail.com>
2024-04-04 11:24:49 +0700

    Refactor Main class and add new test class

--- semester2/test1.java
+++ semester2/test1.java
@@ -1,12 +1,15 @@
-public class test1 {
-
-    public static void main(String[] args) {
-        try {
-            int a = 1;
-            int b = 0;
-            int c = a / b;
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
+public interface test1 {
+    private void test() {
+        System.out.println("test1");
     }
+
+    protected void test2() {
+        System.out.println("test2");
+    }
+
+    public void test3() {
+        System.out.println("test3");
+    }
+
+    void test4();
 }
