ef72d1beb23399e7d295d74c3e27b0c64c6fb816
Nathan Sudiara <nathansudiara@gmail.com>
2024-04-08 22:50:40 +0700

    Refactor BasePlusCommissionEmployee class to extend CommissionEmployee and add necessary methods

--- semester2/OOP/Praktikum/polymorfisme/BasePlusCommissionEmployee.java
+++ semester2/OOP/Praktikum/polymorfisme/BasePlusCommissionEmployee.java
@@ -1,5 +1,27 @@
 package OOP.Praktikum.polymorfisme;
 
-public class BasePlusCommissionEmployee {
+public class BasePlusCommissionEmployee extends
+        CommissionEmployee {
+    private double baseSalary;// gaji pokok tiap minggu
 
+    public BasePlusCommissionEmployee(String name, String noKTP, double sales, double rate, double salary) {
+        super(name, noKTP, sales, rate);
+        setBaseSalary(salary);
+    }
+
+    public void setBaseSalary(double salary) {
+        baseSalary = salary;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public double earnings() {
+        return getBaseSalary() + super.earnings();
+    }
+
+    public String toString() {
+        return String.format("Base-Salaried " + super.toString() + "\nbase salary " + getBaseSalary());
+    }
 }
