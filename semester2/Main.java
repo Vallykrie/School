5c2081f8575b3a9784bbf1efab60fb9be30a808b
Nathan Sudiara <nathansudiara@gmail.com>
2024-04-23 19:18:38 +0700

    Refactor launch.json and add .idea configuration files

--- semester2/Main.java
+++ semester2/Main.java
@@ -1,34 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
-
-        BinarySearchTree myBST = new BinarySearchTree();
-
-        // myBST.insert(13);
-        // myBST.insert(11);
-        // myBST.insert(16);
-        // myBST.insert(9);
-        // myBST.insert(12);
-        // myBST.insert(15);
-        // myBST.insert(18);
-        // myBST.insert(10);
-        // myBST.insert(14);
-        // myBST.insert(17);
-        // myBST.insert(19);
-
-        // myBST.insert(13);
-        // myBST.insert(11);
-        // myBST.insert(16);
-        // myBST.insert(9);
-        // myBST.insert(12);
-        // myBST.insert(18);
-        // myBST.insert(10);
-        // myBST.insert(17);
-        // myBST.insert(19);
-
-        System.out.println(myBST.DFSPreOrder());
-        System.out.println(myBST.DFSInOrder());
-        System.out.println(myBST.DFSPostOrder());
+        test t1 = new test();
+        test t2 = new test();
+        test t3 = new test();
+        System.out.println(test.count);
     }
-
 }
--- semester2/test.java
+++ semester2/test.java
@@ -1,19 +1,23 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * test
  */
 public class test {
-    static int count;
-
-    test() {
-        count++;
-    }
-}
-
-class Main {
     public static void main(String[] args) {
-        test t1 = new test();
-        test t2 = new test();
-        test t3 = new test();
-        System.out.println(test.count);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < 10; i++) {
+            int num = Math.random()*10+1;
+            minHeap.add(num);
+            maxHeap.add(num);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(minHeap.peek());
+            System.out.println(maxHeap.peek());
+        }
+//        minHeap.remove()
     }
 }
